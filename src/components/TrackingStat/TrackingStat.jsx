640a251dfdf2a43a345932ffccd092396df1bc94
Max Kameniev <mkamen@softserveinc.com>
2019-09-04 11:41:58 +0300

    created statistics component

--- src/components/TrackingStat/TrackingStat.jsx
+++ src/components/TrackingStat/TrackingStat.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { calcConsts, status } from '../../constants/text';
 import style from './TrackingStat.css';
 
-export const TrackingStat = (data) => {
-    console.log(data);
-    return (
-        <div>
+export const TrackingStat = ({ data, totalSum }) => {
+    const {backMoneyTitle, backSum, stateField, shipedParcel, shipedParcelTitle} = calcConsts;
 
+    return (
+        <div className={style.wrapper}>
+            <div className={style.details}>
+                {backMoneyTitle} - {totalSum(data, backSum, stateField, status.parcelRecived)};
+            </div>
+            <div className={style.details}>
+                {shipedParcelTitle} - {totalSum(data, shipedParcel, stateField, status.parcelRecived)};
+            </div>
         </div>
     );
 };
